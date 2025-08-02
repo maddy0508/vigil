4f2403d180a7d4ebf24bfad7a07240f663ad0d12
Maddy McKellar <mckellarmaddy@gmail.com>
2025-08-02 14:20:39 +0000

    one more thing, I'd like for this to be the logo

--- src/components/vigil-logo.tsx
+++ src/components/vigil-logo.tsx
@@ -5,26 +5,20 @@ export function VigilLogo(props: SVGProps<SVGSVGElement>) {
     <svg
       width="24"
       height="24"
-      viewBox="0 0 24 24"
+      viewBox="0 0 48 48"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <title>Vigil Logo</title>
-      <path
-        d="M12 2.16333L3.52502 5.28833V11.1633C3.52502 16.2373 6.94502 20.9853 12 22.8363C17.055 20.9853 20.475 16.2373 20.475 11.1633V5.28833L12 2.16333Z"
-        className="stroke-primary"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M8.5 10.5L12 14L15.5 10.5"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+        <title>Vigil Logo</title>
+        <path d="M29.492 22.197L23.033 28.623L16.51 22.197L15.703 14.883L23.033 14.883L30.242 14.883L29.492 22.197Z" className="stroke-primary" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M19.168 18.547C19.168 18.547 18.273 19.328 17.727 19.328C17.18 19.328 16.57 18.547 16.57 18.547" className="stroke-primary" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M29.492 18.547C29.492 18.547 28.598 19.328 28.051 19.328C27.504 19.328 26.895 18.547 26.895 18.547" className="stroke-primary" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M23.033 44C23.033 44 10.025 36.328 10.025 24.32C10.025 12.312 23.033 4 23.033 4C23.033 4 36.041 12.312 36.041 24.32C36.041 36.328 23.033 44 23.033 44Z" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M31.365 24.111L23.033 28.623" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M15.467 24.234L23.033 28.623" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M23.033 28.623L23.033 33.68" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M20.502 14.883L23.033 11.094L25.564 14.883" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   );
 }
